466e9771f0aacc180306aa709c4023d0cfff71b0
Filipe Almeida <filipecalm@gmail.com>
2024-01-28 16:39:41 -0300

    feat: Category routes

---
+++ backend/src/routes/CategoryRoutes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import CategoryController from '../controllers/CategoryController';
+
+// middlewares
+import verifyToken from '../middlewares/verify-token';
+import verifyAdmin from '../middlewares/verify-admin';
+
+const routes = Router();
+
+routes.post('/', verifyToken, verifyAdmin, CategoryController.createCategory);
+routes.get('/', CategoryController.listCategories);
+routes.get('/:id', verifyToken, verifyAdmin, CategoryController.getOne);
+routes.patch('/:id', verifyToken, verifyAdmin, CategoryController.updateCategory);
+routes.delete('/:id', verifyToken, verifyAdmin, CategoryController.deleteCategory);
+
+export default routes;
