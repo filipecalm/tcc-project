d04d244484fc6093edcbc81c76e4a5da60b1a4d6
Filipe Almeida <filipecalm@gmail.com>
2024-01-28 16:36:36 -0300

    feat: OrdersProduct model

---
+++ backend/src/models/OrdersProduct.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const OrdersProductSchema = new mongoose.Schema({
+  productsId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product'
+  },
+  amount: {
+    type: Number,
+    require: true
+  },
+  price: {
+    type: Number,
+    require: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  orderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Orders'
+  }
+})
+
+const OrdersProduct = mongoose.model('OrdersProduct', OrdersProductSchema)
+
+export default OrdersProduct;
---
+++ backend/src/models/index.ts
@@ -0,0 +1,7 @@
+import User from './User';
+import Product from './Product';
+import Category from './Category';
+import Orders from './Orders';
+import OrdersProduct from './OrdersProduct';
+
+export { User, Product, Category, Orders, OrdersProduct };
