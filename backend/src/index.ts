38925bc73176b16930bf5bdfcaf78bfe6f8e9704
Filipe Almeida <filipecalm@gmail.com>
2024-02-25 16:21:45 -0300

    fix: api config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,16 +7,33 @@ dotenv.config();
 
 const app: Express = express();
 
-app.use(
-  cors({ origin: `http://${process.env.HOST}:${process.env.CLIENT_PORT}` })
-);
+let corsOrigin: string;
+if (process.env.NODE_ENV === 'dev') {
+  corsOrigin = `http://${process.env.HOST}:${process.env.CLIENT_PORT}`;
+} else if (process.env.NODE_ENV === 'prod') {
+  corsOrigin = process.env.REACT_APP_CLIENT_URL || '*';
+} else {
+  throw new Error('O ambiente não está definido corretamente');
+}
+
+app.use(cors({
+  origin: '*',
+  methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS", 
+  allowedHeaders: '*',
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
+
+// Ajuste a política de referência como uma configuração separada, pois não é parte do CORS
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Referrer-Policy', 'no-referrer'); // Definir política de referência
+  next();
+});
 
 // Increase upload limit to 50mb
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-// Importando a conexão com o banco de dados
-import conn from './database/db';
 import handleError from './helpers/handleError';
 
 // Public Images
@@ -28,8 +45,6 @@ import UserRoutes from './routes/UserRoutes';
 import CategoryRoutes from './routes/CategoryRoutes';
 import CartRoutes from './routes/CartRoutes';
 
-app.use(express.json());
-
 app.use('/product', ProductRoutes);
 app.use('/user', UserRoutes);
 app.use('/category', CategoryRoutes);
@@ -37,6 +52,11 @@ app.use('/cart', CartRoutes);
 
 app.use((err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
   handleError(err, req, res, next);
+});
+
+// Definição da rota raiz
+app.get('/', (req: Request, res: Response) => {
+  res.json('Bem-vindo à minha API!');
 });
 
 connectToDatabase().then(() => {
