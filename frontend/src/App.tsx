198597a1476ec98337f5b1defcbfcd2b73a03be2
Filipe Almeida <filipecalm@gmail.com>
2024-02-25 16:32:56 -0300

    feat: login status

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,55 +10,57 @@ import Register from './pages/Register';
 import AboutPage from './pages/AboutPage';
 import EditProfile from './pages/EditProfile';
 import Protected from './components/Protected';
+import LoginStatusListener from './utils/utilLogin'
 
 function App() {
   const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
   const isAdminUser = localStorage.getItem('isAdmin') === 'true';
 
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Products />} />
-          <Route path="/product/category/:id" element={<Products />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/sobre" element={<AboutPage />} />
-          <Route
-            path="/success/:id"
-            element={
-              <Protected isLoggedIn={isLoggedIn}>
-                <Success />
-              </Protected>
-            }
-          />
-          <Route
-            path="/cart"
-            element={
-              <Protected isLoggedIn={isLoggedIn}>
-                <Requests />
-              </Protected>
-            }
-          />
-          <Route
-            path="/editprofile"
-            element={
-              <Protected isLoggedIn={isLoggedIn}>
-                <EditProfile />
-              </Protected>
-            }
-          />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/admin"
-            element={
-              <Protected isLoggedIn={isAdminUser}>
-                <Admin />
-              </Protected>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <LoginStatusListener />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Products />} />
+        <Route path="/product/category/:id" element={<Products />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/sobre" element={<AboutPage />} />
+        <Route
+          path="/success/:id"
+          element={
+            <Protected isLoggedIn={isLoggedIn}>
+              <Success />
+            </Protected>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <Protected isLoggedIn={isLoggedIn}>
+              <Requests />
+            </Protected>
+          }
+        />
+        <Route
+          path="/editprofile"
+          element={
+            <Protected isLoggedIn={isLoggedIn}>
+              <EditProfile />
+            </Protected>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/admin"
+          element={
+            <Protected isLoggedIn={isAdminUser}>
+              <Admin />
+            </Protected>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
