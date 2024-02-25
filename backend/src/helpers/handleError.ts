107fe56809bca369b333d75e15f694602578051f
Filipe Almeida <filipecalm@gmail.com>
2024-02-25 16:26:27 -0300

    fix: env error

--- backend/src/helpers/handleError.ts
+++ backend/src/helpers/handleError.ts
@@ -1,14 +1,19 @@
 import { UnauthorizedError } from 'express-jwt'
 import { ValidationError } from 'express-validation'
 import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
-
+import MESSAGE from '../constants/messages';
 export default (error:ErrorRequestHandler , req: Request, res: Response, next: NextFunction) => {
   if (error instanceof ValidationError) {
     return res.status(error.statusCode).json(error)
   }
 
   if (error instanceof UnauthorizedError) {
-    return res.status(error.status).json(error)
+    return res.status(error.status || 401).json({ message: MESSAGE.ERROR.ACCESS_DENIED });
   }
-  return res.status(500).json(error)
+
+  if (process.env.NODE_ENV === 'dev') {
+    return res.status(500).json({ message: MESSAGE.ERROR.ERROR_CATCH });
+  } else {
+    return res.status(500).json({ message: MESSAGE.ERROR.ERROR_CATCH });
+  }
 }
